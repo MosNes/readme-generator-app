d5b2155839f72f8a9f8e69c27f4e35eaaaf3381b
MosNes <7hekarl@gmail.com>
2022-06-28 15:14:19 -0400

    added code to renderLicenseBadge and renderLicenseSection functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,7 +6,7 @@ const testData = {
   contribute: "These are the instructions for making contributions.",
   tests: "These are the instructions for testing.",
   confirmLicense: true,
-  license: "MIT",
+  license: "GNU GPLv3",
   github: "MosNes",
   repoLink: "https://github.com/MosNes/readme-generator-app",
   email: "7hekarl@gmail.com"
@@ -14,25 +14,47 @@ const testData = {
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license.confirmLicense) {
+    return '';
+  } else {
+    return `
+      [license](https://img.shields.io/badge/License-${license.license}-brightgreen)
+    `
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license.confirmLicense) {
+    return '';
+  } else {
+    return `
+    ## License
+    
+    This project is covered under the ${license.license} license.
+    `
+  }
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  //gets the github repo name from the end of the github repo URL
   let userRepo = data.repoLink.split('/')[4];
   return `
   # ${data.title}
 
   ## Badges
   
-  ${renderLicenseBadge()}
+  ${renderLicenseBadge(data)}
   
   ![${userRepo}](https://img.shields.io/github/languages/top/${data.github}/${userRepo})
   
@@ -71,6 +93,8 @@ function generateMarkdown(data) {
   
   - GitHub: [${data.github}](https://github.com/${data.github})
   - Email: ${data.email}
+
+  ${renderLicenseSection(data)}
 `;
 };
 
